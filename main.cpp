47a552f176db1f802de6f7927c04cba188ab49b5
VuDuong-Sketch <errolne1309@gmail.com>
2025-02-21 16:38:29 +0700

    V

--- main.cpp
+++ main.cpp
@@ -1,65 +1,114 @@
-#include <SDL2/SDL.h>
 #include <iostream>
+#include <SDL.h>
+#include <SDL2/SDL_image.h>
 
 using namespace std;
 
 const int SCREEN_WIDTH = 800;
-
 const int SCREEN_HEIGHT = 600;
+const char* WINDOW_TITLE = "Hello World!";
 
-const string WINDOW_TITLE = "Paint";
-
-void initSDL(SDL_Window* &window, SDL_Renderer* &renderer){
-	
-	window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-
-	SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP;
-
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	
-	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-	
-	SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+void logErrorAndExit(const char* msg, const char* error)
+{
+    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
+    SDL_Quit();
 }
 
-void quitSDL(SDL_Window* window, SDL_Renderer* renderer){
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        logErrorAndExit("SDL_Init", SDL_GetError());
+
+    SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    //full screen
+    //window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
+    if (window == NULL) logErrorAndExit("CreateWindow", SDL_GetError());
+
+    if (!IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG))
+        logErrorAndExit( "SDL_image error:", IMG_GetError());
+
+    return window;
 }
 
-void waitUntilKeyPressed(){
-	SDL_Event e;
-	for(;;){
-		if ( SDL_WaitEvent(&e) != 0 && (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) ) return;
-		SDL_Delay(100);
-	}
+SDL_Renderer* createRenderer(SDL_Window* window)
+{
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+                                              SDL_RENDERER_PRESENTVSYNC);
+    //Khi ch?y trong máy ?o (ví d? phòng máy ? tru?ng)
+    //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
+
+    if (renderer == NULL) logErrorAndExit("CreateRenderer", SDL_GetError());
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    return renderer;
 }
 
-int main( int argc, char* argv[] ){	
-	SDL_Window* window;
-	SDL_Renderer* renderer;
-	initSDL(window, renderer);
-	// Your drawing code here
-	SDL_SetRenderDrawColor(renderer, 255, 127, 0, 0);
-	for(int i = 0; i <= 600; i++){
-		for(int j = 0; j <= 600; j++){
-			if((i-100)*(i-100) + (j-100)*(j-100) <= 10000) SDL_RenderDrawPoint(renderer, i, j);
-		}
-	}
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-	for(int j = 0; j <= 600; j++){
-		SDL_RenderDrawPoint(renderer, 601, j);
-	}
-	SDL_SetRenderDrawColor(renderer, 255, 127, 0, 0);
-	for(int i = 602; i <= 800; i++){
-		for(int j = 0; j <= 600; j++){
-			SDL_RenderDrawPoint(renderer, i, j);
-		}
-	}
-	SDL_RenderPresent(renderer);
-	// use SDL_RenderPresent(renderer) to show it
-	waitUntilKeyPressed();
-	quitSDL(window, renderer);
-	return 0;
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
+{
+    IMG_Quit();
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+void waitUntilKeyPressed()
+{
+    SDL_Event e;
+    while (true) {
+        if ( SDL_PollEvent(&e) != 0 &&
+             (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) )
+            return;
+        SDL_Delay(100);
+    }
+}
+
+void renderTexture(SDL_Texture *texture, int x, int y, SDL_Renderer* renderer)
+{
+	SDL_Rect dest;
+
+	dest.x = x;
+	dest.y = y;
+	SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
+
+	SDL_RenderCopy(renderer, texture, NULL, &dest);
+}
+
+SDL_Texture *loadTexture(const char *filename, SDL_Renderer* renderer)
+{
+	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
+
+	SDL_Texture *texture = IMG_LoadTexture(renderer, filename);
+	if (texture == NULL)
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load texture %s", IMG_GetError());
+
+	return texture;
+}
+
+int main(int argc, char *argv[])
+{
+    SDL_Window* window = initSDL(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+    SDL_Renderer* renderer = createRenderer(window);
+
+    SDL_Texture* background = loadTexture("bikiniBottom.jpg", renderer);
+    SDL_RenderCopy( renderer, background, NULL, NULL);
+
+    SDL_RenderPresent( renderer );
+    waitUntilKeyPressed();
+
+    SDL_Texture* spongeBob = loadTexture("Spongebob.png", renderer);
+    renderTexture(spongeBob, 200, 200, renderer);
+
+    SDL_RenderPresent( renderer );
+    waitUntilKeyPressed();
+
+    SDL_DestroyTexture( spongeBob );
+    spongeBob = NULL;
+    SDL_DestroyTexture( background );
+    background = NULL;
+
+    quitSDL(window, renderer);
+    return 0;
 }
