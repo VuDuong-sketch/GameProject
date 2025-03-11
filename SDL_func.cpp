59a7a42d20edc49cefc4c46e9ecaaca190cb0f8d
VuDuong-sketch <errolne1309@gmail.com>
2025-03-11 14:19:13 +0700

    V

--- SDL_func.cpp
+++ SDL_func.cpp
@@ -27,42 +27,4 @@ void waitUntilPressed(){
 	}
 }
 
-void renderTexture(SDL_Texture *texture, int x, int y, SDL_Renderer* renderer){
-	
-	SDL_Rect dest;
 
-	dest.x = x;
-	dest.y = y;
-	SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
-
-	SDL_RenderCopy(renderer, texture, NULL, &dest);
-}
-
-SDL_Texture* loadTexture(const char* filename, SDL_Renderer* renderer){
-	
-	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
-
-	SDL_Texture* texture = IMG_LoadTexture(renderer, filename);
-	if (texture == NULL)
-        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load texture %s", IMG_GetError());
-
-	return texture;
-}
-
-void move () {
-	bool quit = false;
-    SDL_Event event;
-	while (!quit) {
-        //Handle events on queue
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) quit = true;
-        }
-
-        const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
-
-        if (currentKeyStates[SDL_SCANCODE_UP] ) break;
-        if (currentKeyStates[SDL_SCANCODE_DOWN] ) break;
-        if (currentKeyStates[SDL_SCANCODE_LEFT] ) break;
-        if (currentKeyStates[SDL_SCANCODE_RIGHT] ) break;
-    }
-}
